4b7ca1009b3a462a611b60814773d5a468cc6a51
ICE0208 <doky3307@naver.com>
2025-02-15 23:27:11 +0900

    about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,35 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { siteConfig } from "@/config/site";
+
 export default function AboutPage() {
-  return <div>About</div>;
+  return (
+    <div className="mx-auto px-8 max-w-6xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-x-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">
+            About Me
+          </h1>
+        </div>
+      </div>
+      <hr className="my-8" />
+      <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
+        <div className="min-w-48 max-w-48 flex flex-col gap-2">
+          <Avatar className="size-48">
+            <AvatarImage
+              src="https://avatars.githubusercontent.com/u/46257328"
+              alt={siteConfig.name}
+            />
+            <AvatarFallback />
+          </Avatar>
+          <h2 className="text-2xl font-bold text-center break-words">
+            {siteConfig.author}
+          </h2>
+          <p className="text-muted-foreground text-center break-words">
+            Software Developer
+          </p>
+        </div>
+        <p className="text-muted-foreground text-lg py-4 md:py-6">Hello~</p>
+      </div>
+    </div>
+  );
 }
