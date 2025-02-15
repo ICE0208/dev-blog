15007b0032dd8c6ba4afa812fa2ebd2479734478
ICE0208 <doky3307@naver.com>
2025-02-16 00:07:36 +0900

    dynamic graph image

---
+++ src/app/api/og/route.tsx
@@ -0,0 +1,52 @@
+import { NextRequest } from "next/server";
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+
+export async function GET(req: NextRequest) {
+  const interBold = fetch(
+    new URL("../../../../assets/fonts/Inter-Bold.ttf", import.meta.url)
+  ).then((res) => res.arrayBuffer());
+  try {
+    const fontBold = await interBold;
+    const { searchParams } = req.nextUrl;
+    const title = searchParams.get("title");
+    if (!title) {
+      return new Response("No title provided", { status: 500 });
+    }
+    const heading =
+      title.length > 140 ? `${title.substring(0, 140)}...` : title;
+
+    return new ImageResponse(
+      (
+        <div
+          style={{
+            height: "100%",
+            width: "100%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "#fff",
+            fontSize: 28,
+            fontWeight: 600,
+
+            textAlign: "center",
+          }}
+        >
+          <div style={{ fontSize: 64 }}>{heading}</div>
+          <div style={{ marginTop: 40 }}>{"🧊 ICE28's Dev Blog"}</div>
+        </div>
+      ),
+      {
+        width: 1200,
+        height: 630,
+        fonts: [
+          { name: "Inter", data: fontBold, weight: 700, style: "normal" },
+        ],
+      }
+    );
+  } catch {
+    return new Response("Failed to generate image", { status: 500 });
+  }
+}
